fe56386276e7e5c2b033f22412f6c55127243c8b
Liderk <vismoke@yandex.ru>
2020-11-22 04:33:03 +0700

    add json response

--- teddy_food/animals/views.py
+++ teddy_food/animals/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.generic import View
+
 from .models import City, Shelter, Breed, Kind, Animal, OrderNumber, Lemmas, \
-    Transaction
+  Transaction
 
 
-def index(request):
-    city = City.objects.filter(city='Самара')
-    animals = Animal.objects.filter(city__in=city)
-    context = {'animals': animals}
-    return render(request, 'index.html', context)
+class Index(View):
+    def get(self, request):
+        context = []
+        girls = Animal.objects.filter(sex='девочка').count()
+        girls_home = Animal.objects.filter(sex='девочка', home=1).count()
+        cat_girl = {'sex': 'девочка', 'total': girls, 'home': girls_home}
+        context.append(cat_girl)
+        boys = Animal.objects.filter(sex='мальчик').count()
+        boys_home = Animal.objects.filter(sex='мальчик', home=1).count()
+        cat_boy = {'sex': 'мальчик', 'total': boys, 'home': boys_home}
+        context.append(cat_boy)
+
+        return JsonResponse(context, safe=False)
