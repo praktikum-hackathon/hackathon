dc05072e73faff997cc17c84dd2c0444fa4b56a9
Liderk <vismoke@yandex.ru>
2020-11-22 11:23:23 +0700

    add sqlite3.db

--- teddy_food/animals/urls.py
+++ teddy_food/animals/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('data/', views.Data.as_view(), name='data'),
+    path('sex/', views.SexAnalize.as_view(), name='sexanalize'),
     path('', views.index, name='index'),
 
 ]
--- teddy_food/animals/views.py
+++ teddy_food/animals/views.py
@@ -10,15 +10,32 @@ from .models import City, Shelter, Breed, Kind, Animal, OrderNumber, Lemmas, \
 class Data(View):
     def get(self, request):
         context = []
-        girls = Animal.objects.filter(sex='девочка').count()
-        girls_home = Animal.objects.filter(sex='девочка', home=1).count()
-        cat_girl = {'sex': 'Девочка', 'total': girls, 'home': girls_home}
+        kind = Kind.objects.filter(title='Кошки')
+        girls = Animal.objects.filter(sex='девочка', kind__in=kind)
+        girls_all = girls.count()
+        girls_home = girls.filter(home=1).count()
+        cat_girl = {'sex': 'Девочка', 'total': girls_all, 'home': girls_home}
         context.append(cat_girl)
-        boys = Animal.objects.filter(sex='мальчик').count()
-        boys_home = Animal.objects.filter(sex='мальчик', home=1).count()
-        cat_boy = {'sex': 'Мальчик', 'total': boys, 'home': boys_home}
+        boys = Animal.objects.filter(sex='мальчик', kind__in=kind).all()
+        boys_all = boys.count()
+        boys_home = boys.filter(home=1).count()
+        cat_boy = {'sex': 'Мальчик', 'total': boys_all, 'home': boys_home}
         context.append(cat_boy)
 
+        return JsonResponse(context, safe=False)
+
+
+class SexAnalize(View):
+    def get(self, request):
+        context = []
+        kind = Kind.objects.filter(title='Кошки')
+        lemmas = Lemmas.objects.filter(lemmas='игрушка')
+        boys = Animal.objects.filter(sex='мальчик', kind__in=kind).all()
+        transactions = Transaction.objects.filter(
+          lemmas__in=lemmas,
+          animal__in=boys).count()
+        game = {'transactions':transactions}
+        context.append(game)
         return JsonResponse(context, safe=False)
 
 
